1d80d48c7ec273a941d626ea5cd017372091aa49
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-04-25 12:42:46 -0300

    implemented bonus

--- index.js
+++ index.js
@@ -13,24 +13,38 @@ app.post("/sign-up", (req, res) => {
 
   if (data.username && data.avatar) {
     users.push(data);
-    res.status(200).send("OK");
+    res.status(201).send({ data, msg: "OK" });
   } else {
-    res.status(400).send(`Bad request`);
+    res.status(400).send("Todos os campos são obrigatórios!");
   }
 });
+
 app.post("/tweets", (req, res) => {
   const data = req.body;
+  const username = req.headers.username;
 
-  if (data.username && data.tweet) {
-    tweets.push(data);
-    res.status(200).send("OK");
+  if (username && data.tweet) {
+    tweets.push({ username, tweet: data.tweet });
+    res.status(201).send({ data, msg: "OK" });
   } else {
-    res.status(400).send("Bad request");
+    res.status(400).send("Todos os campos são obrigatórios!");
   }
 });
 
-app.get("/tweets", (_req, res) => {
-  res.send(tweets.slice(-10));
+app.get("/tweets", (req, res) => {
+  let page = req.query.page;
+  page = page ? parseInt(page) : null;
+
+  if (!page) res.send(tweets.slice(-10));
+  else if (isNaN(page) || page < 1) {
+    res.status(400).send("Informe uma página válida!");
+  } else {
+    res.send(tweets.slice((page - 1) * 10, page * 10));
+  }
+});
+
+app.get("/tweets/:username", (req, res) => {
+  res.send(tweets.filter((tweet) => tweet.username === req.params.username));
 });
 
 app.listen(5000, () => console.log("Listening on 5000"));
